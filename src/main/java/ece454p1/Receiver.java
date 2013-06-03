9a49f67a918f6f6592e97a82a79cf04721ca4cc4
Alexander Georgie <ageorgie@ECELinux10.uwaterloo.ca>
2013-06-03 19:47:26 -0400

    Fixes socket code - gets filemap transferring

--- src/main/java/ece454p1/Receiver.java
+++ src/main/java/ece454p1/Receiver.java
@@ -3,12 +3,14 @@ package ece454p1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
 import java.util.BitSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
@@ -31,30 +33,38 @@ public class Receiver implements Callable<Integer> {
             while(true) {
     //            System.out.println(serverSocket.isClosed());
                 Socket client = serverSocket.accept();
-                ObjectInputStream ois;
+								System.out.println("Am I here?");
                 Object obj = new Object();
-		
-                SocketChannel channel = client.getChannel();
-		if(channel==null) {
-		    continue;
-		}
+								
+								InputStream is = client.getInputStream();
+								ObjectInputStream ois = new ObjectInputStream(is);
+								
+								System.out.println("I am still working at this point");
                 try {
-                    ois = new ObjectInputStream(Channels.newInputStream(channel));
+                		System.out.println("Can I read the object?");
                     obj = ois.readObject();
+                		System.out.println("I might have read the object");
+                		System.out.println(obj.getClass());
                 } catch (Exception ex) {
+                		System.out.println("exception was caught");
                     ex.printStackTrace();
                 }
 
                 if(obj.getClass().isAssignableFrom(Chunk.class)) {
+                    System.out.println("Received Chunk!");
                     Chunk chunk = (Chunk) obj;
                     Peer.ReceiveChunk(chunk);
-                } else if (obj.getClass().isAssignableFrom(Map.class)) {
+                } else if (obj.getClass().isAssignableFrom(HashMap.class)) {
+                		System.out.println("Received FileMap");
                     Map<String, Map<String, BitSet>> bitSetMap = (Map<String, Map<String, BitSet>>) obj;
+                    System.out.println(bitSetMap);
                 } else {
+                		System.out.println("That's a cold ass honky");
                     throw new Exception("Received object type is not recognized");
                 }
             }
         } finally {
+        		System.out.println("Did the socket close?");
             serverSocket.close();
         }
     }
