b23e5e71cccf9eb565020c30a8040d69858f8b1e
Omid Mortazavi <omidmortazavi@Omids-MacBook-Air.local>
2013-06-07 07:12:13 -0400

    yet another commit

--- src/main/java/ece454p1/Receiver.java
+++ src/main/java/ece454p1/Receiver.java
@@ -35,6 +35,7 @@ public class Receiver implements Callable<Integer> {
         try {
             while(true) {
                 Socket client = serverSocket.accept();
+                Thread.sleep(100);
                 System.out.printf("Receiver: accepted new connection. Delegating to Updater.\n");
                 executorService.submit(new Updater(client));
             }
