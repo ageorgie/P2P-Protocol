23601a55f1d294214d0fd5650c67d0ef4b634a39
Omid Mortazavi <omidmortazavi@Omids-MacBook-Air.local>
2013-06-07 03:16:54 -0400

    yet another commit

--- src/main/java/ece454p1/Updater.java
+++ src/main/java/ece454p1/Updater.java
@@ -29,7 +29,9 @@ public class Updater implements Callable<Integer> {
         InputStream is = socket.getInputStream();
         System.out.printf("Accepted incoming connection from %s\n", socket.getInetAddress().getHostName());
         String senderHostName = socket.getInetAddress().getHostName().toLowerCase();
+        System.out.printf("senderhostname: %s\n", senderHostName);
         int senderPort = Peer.getPeers().getPort(senderHostName);
+        System.out.printf("senderport: %d\n", senderPort);
         Peer.getPeers().setConnectionState(String.format("%s %d", senderHostName, senderPort), true);
         System.out.printf("Connection State Map: %s\n", Peer.getPeers().getConnectionStateMap());
         ObjectInputStream ois = new ObjectInputStream(is);
